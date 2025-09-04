1f7e9abc9588259ad277a560a067399062474779
Luke Barrett <ld.barrett81@gmail.com>
2025-09-04 14:52:54 -0400

    Updated Script to accept multiple filenames as input and generate a .tsv file where the abundance of each amino acid is listed in alphabetical order in a tab separated format

--- miniproject-codon-usage/codon-usage.py
+++ miniproject-codon-usage/codon-usage.py
@@ -4,34 +4,61 @@ import sys
 import fasta
 import codons
 
-# print (codons.forward)
-aas = {}
+aas = {} # Parent dictionary; Keys will be the filename of the argument I input, values will be the abundance dictionary for that FASTA file
+fileCount = 1
+# Credit to Grayden for this idea, I would have otherwise tried to define the other abundance dictionaries for
+# each file from the get-go, before iterating through sys.argv.
 
-fs = open(sys.argv[1])
-identities = [] # Using this to count how many contigs there are in a file. This should match the number of stop codons are in a given library.
-totallength = 0
+for filename in sys.argv:
+    if filename.startswith('./'):
+        continue
+    aas[filename] = {}
 
-for ident, sequence in fasta.FASTAReader(fs):
-    # print(ident)
-    # print(len(sequence))
-    totallength += len(sequence)
-    identities.append(ident)
-    for i in range(0,len(sequence),3):
-        codon = sequence[i:i+3]
-        # print(codon)
-        if codon in codons.forward:
-            if codons.forward[codon] in aas:
-                aas[codons.forward[codon]] += 1
-            else:
-                aas[codons.forward[codon]] = 1
+    # print (codons.forward)
 
-sorted_aas = dict(sorted(aas.items())) # I sorted because the dictionary key order might vary 
-                                       # across FASTA files because the order that amino acids are added 
-                                       # to the abundance dictionary is sequence dependent
+    fs = open(filename)
+    # identities = [] # Using this to count how many contigs there are in a file. This should match the number of stop codons are in a given library.
+    # totallength = 0
 
-print(totallength)
-# print(sorted_aas)
-# print(len(identities)) # Check how many contigs in a file, this should match stop codon count for membrane.fa and cytoplasm.fa files, since they're all full, in-frame coding sequences
+    for ident, sequence in fasta.FASTAReader(fs):
+        # print(ident)
+        # print(len(sequence))
+        # totallength += len(sequence)
+        # identities.append(ident)
+        for i in range(0,len(sequence),3):
+            codon = sequence[i:i+3]
+            # print(codon)
+            if codon in codons.forward:
+                if codons.forward[codon] in aas[filename]:
+                    aas[filename][codons.forward[codon]] += 1
+                else:
+                    aas[filename][codons.forward[codon]] = 1
 
-    
+    sorted_aas = dict(sorted(aas[filename].items())) # I sorted because the dictionary key order might vary 
+                                        # across FASTA files because the order that amino acids are added 
+                                        # to the abundance dictionary is sequence dependent
+    aas[filename] = sorted_aas
+
+    # print(totallength)
+    # print(aas[filename])
+    # print(len(identities)) # Check how many contigs in a file, this should match stop codon count for membrane.fa and cytoplasm.fa files, since they're all full, in-frame coding sequences
+
+output = open('cyto-v-mem.tsv','w')
+
+single_letter_aminoacids = codons.reverse.keys()
+sortedSingleLetterCodes = sorted(single_letter_aminoacids)
+# print(sortedSingleLetterCodes)
+
+for letter in sortedSingleLetterCodes:
+    output.write(f"{letter}\t")
+    for abundanceCounts in aas:
+        output.write(str(aas[abundanceCounts][letter]))
+        output.write('\t')
+    output.write('\n')
+
+
+
+output.close()
+
         
+            
